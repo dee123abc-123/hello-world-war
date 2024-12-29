7fba8580c147d0342042f2390d62284c943d73af
dee123abc-123 <deepakpadmanabhan86@gmail.com>
2024-12-29 11:05:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ -d hello-world-war ]
 then
 rm -rf *
 fi
-git clone https://github.com/shrikantashetty/hello-world-war.git
+git clone https://github.com/dee123abc-123/hello-world-war.git
 cd hello-world-war
 mvn clean package
 ls
